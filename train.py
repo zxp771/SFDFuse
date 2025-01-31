5ebdf363cf05cbafb48c5501316ac986417c519d
zxp771 <35118859+zxp771@users.noreply.github.com>
2025-01-31 13:26:15 +0800

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ Import packages
 ------------------------------------------------------------------------------
 '''
 
-from net_fft_detail_01201 import Restormer_Encoder, Restormer_Decoder_Phase,DetailFeatureExtraction,BaseFeatureExtraction_Pool,FeatureInteractionBlock
+from net import Encoder, Decoder, FeatureInteractionBlock
 from utils.dataset import H5Dataset
 import os
 
@@ -17,7 +17,7 @@ import datetime
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from utils.loss_fft import Fusionloss, cc, waveletloss,L_TV
+from utils.loss import Fusionloss, cc
 import kornia
 import matplotlib.pyplot as plt
 
@@ -29,7 +29,7 @@ Configure our network
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 criteria_fusion = Fusionloss()
-model_str = 'CDDFuse'
+model_str = 'SFDFuse'
 
 # . Set the hyper-parameters for training
 num_epochs = 90 # total epoch
@@ -55,30 +55,20 @@ optim_gamma = 0.5
 
 # Model
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-DIDF_Encoder = nn.DataParallel(Restormer_Encoder()).to(device)
-DIDF_Decoder_Phase = nn.DataParallel(Restormer_Decoder_Phase()).to(device)
-#BaseFuseLayer = nn.DataParallel(BaseFeatureExtraction(dim=64, num_heads=8)).to(device)
-#BaseFuseLayer = nn.DataParallel(BaseFeatureExtraction_Pool(dim=64)).to(device)
+DIDF_Encoder = nn.DataParallel(Encoder()).to(device)
+DIDF_Decoder = nn.DataParallel(Decoder()).to(device)
 BaseFuseLayer = nn.DataParallel(FeatureInteractionBlock(dim=64, num_heads=8,init_fusion=True)).to(device)
-#DetailFuseLayer = nn.DataParallel(DEABlockTrain(conv=default_conv,dim=64,kernel_size=3)).to(device)
-#DetailFuseLayer = nn.DataParallel(WTConv2d(in_channels=64, out_channels=64, kernel_size=3, wt_levels=2)).to(device)
-#DetailFuseLayer = nn.DataParallel(DetailFeatureExtraction(num_layers=1)).to(device)
 DetailFuseLayer = nn.DataParallel(FeatureInteractionBlock(dim=64, num_heads=8,init_fusion=False)).to(device)
-#DetailFuseLayer = nn.DataParallel(FeatureInteractionBlock(dim=64, num_heads=8)).to(device)
-
-#FuseLayer = nn.DataParallel(FeatureInteractionBlock(dim=64, num_heads=8)).to(device)
 
 # optimizer, scheduler and loss function
 optimizer1 = torch.optim.Adam(
     DIDF_Encoder.parameters(), lr=lr, weight_decay=weight_decay)
 optimizer2 = torch.optim.Adam(
-    DIDF_Decoder_Phase.parameters(), lr=lr, weight_decay=weight_decay)
+    DIDF_Decoder.parameters(), lr=lr, weight_decay=weight_decay)
 optimizer3 = torch.optim.Adam(
     BaseFuseLayer.parameters(), lr=lr, weight_decay=weight_decay)
 optimizer4 = torch.optim.Adam(
    DetailFuseLayer.parameters(), lr=lr, weight_decay=weight_decay)
-# optimizer3 = torch.optim.Adam(
-#     FuseLayer.parameters(), lr=lr, weight_decay=weight_decay)
 
 scheduler1 = torch.optim.lr_scheduler.StepLR(optimizer1, step_size=optim_step, gamma=optim_gamma)
 scheduler2 = torch.optim.lr_scheduler.StepLR(optimizer2, step_size=optim_step, gamma=optim_gamma)
@@ -94,8 +84,6 @@ scheduler4 = torch.optim.lr_scheduler.StepLR(optimizer4, step_size=optim_step, g
 MSELoss = nn.MSELoss()
 L1Loss = nn.L1Loss()
 Loss_ssim = kornia.losses.SSIMLoss(11, reduction='mean')
-Wavelet_Loss = waveletloss()
-L_TV = L_TV()
 
 # data loader
 trainloader = DataLoader(H5Dataset(r"data/MSRS_train_imgsize_128_stride_200.h5"),
@@ -122,13 +110,12 @@ for epoch in range(num_epochs):
     for i, (data_VIS, data_IR) in enumerate(loader['train']):
         data_VIS, data_IR = data_VIS.cuda(), data_IR.cuda()
         DIDF_Encoder.train()
-        DIDF_Decoder_Phase.train()
+        DIDF_Decoder.train()
         BaseFuseLayer.train()
         DetailFuseLayer.train()
-        # FuseLayer.train()
 
         DIDF_Encoder.zero_grad()
-        DIDF_Decoder_Phase.zero_grad()
+        DIDF_Decoder.zero_grad()
         BaseFuseLayer.zero_grad()
         DetailFuseLayer.zero_grad()
         # FuseLayer.zero_grad()
@@ -143,8 +130,8 @@ for epoch in range(num_epochs):
             feature_VIS_ll, feature_VIS_detail = DIDF_Encoder(data_VIS)
             feature_I_ll, feature_I_detail = DIDF_Encoder(data_IR)
 
-            data_VIS_hat, feature_vis_hat = DIDF_Decoder_Phase(data_VIS, feature_VIS_ll, feature_VIS_detail)
-            data_IR_hat, feature_ir_hat = DIDF_Decoder_Phase(data_IR, feature_I_ll, feature_VIS_detail)
+            data_VIS_hat, feature_vis_hat = DIDF_Decoder(data_VIS, feature_VIS_ll, feature_VIS_detail)
+            data_IR_hat, feature_ir_hat = DIDF_Decoder(data_IR, feature_I_ll, feature_VIS_detail)
 
             cc_loss_B = cc(feature_VIS_ll, feature_I_ll)
             cc_loss_D = cc(feature_VIS_detail, feature_I_detail)
@@ -155,13 +142,6 @@ for epoch in range(num_epochs):
                                    kornia.filters.SpatialGradient()(data_VIS_hat))
 
             loss_decomp = (cc_loss_D) ** 2 / (1.01 + cc_loss_B)
-
-            #tv_loss = (L_TV(feature_vis_hat)+L_TV(feature_ir_hat))/2
-
-            #Wavelet_loss = Wavelet_Loss(a0,a1,s0,s1)
-            # for name, parameters in Wavelet_Loss.named_parameters():
-            #     print(name, ':', parameters, parameters.size())
-            # print(list(Wavelet_Loss.parameters()))
 
             loss = coeff_mse_loss_VF * mse_loss_V + coeff_mse_loss_IF * \
                    mse_loss_I + coeff_decomp * loss_decomp + coeff_tv * Gradient_loss# + coeff_TV * tv_loss
@@ -176,15 +156,11 @@ for epoch in range(num_epochs):
             feature_VIS_ll, feature_VIS_detail = DIDF_Encoder(data_VIS)
             feature_I_ll, feature_I_detail = DIDF_Encoder(data_IR)
 
-            #feature_F_ll = BaseFuseLayer(feature_VIS_ll + feature_I_ll)
-            #feature_F_detail = DetailFuseLayer(feature_VIS_detail + feature_I_detail)
             feature_F_ll = BaseFuseLayer(feature_VIS_ll,feature_I_ll)
             feature_F_detail = DetailFuseLayer(feature_VIS_detail,feature_I_detail)
             #feature_F_detail = DetailFuseLayer(feature_VIS_detail, feature_I_detail)
 
-
-
-            data_Fuse, feature_F = DIDF_Decoder_Phase(data_VIS, feature_F_ll, feature_F_detail)
+            data_Fuse, feature_F = DIDF_Decoder(data_VIS, feature_F_ll, feature_F_detail)
 
             mse_loss_V = 5 * Loss_ssim(data_VIS, data_Fuse) + MSELoss(data_VIS, data_Fuse)#5*ssim
             mse_loss_I = 5 * Loss_ssim(data_IR, data_Fuse) + MSELoss(data_IR, data_Fuse)
@@ -192,8 +168,7 @@ for epoch in range(num_epochs):
             cc_loss_B = cc(feature_VIS_ll, feature_I_ll)
             cc_loss_D = cc(feature_VIS_detail, feature_I_detail)
             loss_decomp = (cc_loss_D) ** 2 / (1.01 + cc_loss_B)
-            #Wavelet_loss = Wavelet_Loss(a0,a1,s0,s1)
-            #tv_loss = L_TV(feature_F)
+
             fusionloss, _, _= criteria_fusion(data_VIS, data_IR, data_Fuse)
 
             loss = fusionloss + coeff_decomp * loss_decomp# + coeff_TV * tv_loss
@@ -206,8 +181,6 @@ for epoch in range(num_epochs):
                 BaseFuseLayer.parameters(), max_norm=clip_grad_norm_value, norm_type=2)
             nn.utils.clip_grad_norm_(
                DetailFuseLayer.parameters(), max_norm=clip_grad_norm_value, norm_type=2)
-            # nn.utils.clip_grad_norm_(
-            #     FuseLayer.parameters(), max_norm=clip_grad_norm_value, norm_type=2)
 
             optimizer1.step()
             optimizer2.step()
@@ -264,8 +237,8 @@ for epoch in range(num_epochs):
 if True:
     checkpoint = {
         'DIDF_Encoder': DIDF_Encoder.state_dict(),
-        'DIDF_Decoder_Phase': DIDF_Decoder_Phase.state_dict(),
+        'DIDF_Decoder': DIDF_Decoder.state_dict(),
         'BaseFuseLayer': BaseFuseLayer.state_dict(),
         'DetailFuseLayer': DetailFuseLayer.state_dict(),
     }
-    torch.save(checkpoint, os.path.join("models/CDDFuse_" + timestamp + '.pth'))
+    torch.save(checkpoint, os.path.join("models/SFDFuse_" + timestamp + '.pth'))
